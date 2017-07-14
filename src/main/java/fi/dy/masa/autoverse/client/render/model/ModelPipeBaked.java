0b100abceb3a55f3ee79ee3a40fa7373306d8085
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-07-14 17:03:58 +0300

    Pipes: Add proper item model, plus some model clean-up/refactoring

--- src/main/java/fi/dy/masa/autoverse/client/render/model/ModelPipeBaked.java
+++ src/main/java/fi/dy/masa/autoverse/client/render/model/ModelPipeBaked.java
@@ -27,6 +27,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.client.model.ModelStateComposition;
 import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.common.model.TRSRTransformation;
 import fi.dy.masa.autoverse.Autoverse;
@@ -45,31 +46,57 @@ public class ModelPipeBaked implements IBakedModel
     private static final String TEX_SIDE_OUTPUT   = Reference.MOD_ID + ":blocks/pipe_side_output";
 
     private static final EnumFacing[] MODEL_FACES = new EnumFacing[] {
-            EnumFacing.DOWN, EnumFacing.UP,
-            EnumFacing.NORTH, EnumFacing.SOUTH,
-            EnumFacing.WEST, EnumFacing.EAST,
-            null
-    };
+            EnumFacing.DOWN, EnumFacing.UP, EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.WEST, EnumFacing.EAST, null };
 
-    private static final Map<IBlockState, ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>>> QUAD_CACHE = new HashMap<IBlockState, ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>>>();
+    // These are the transforms to get the vertical pipe edge strip model to the correct orientation for each of the pipe edges
+    // From NORTH to NORTH, SOUTH, WEST, EAST
+    private static final TRSRTransformation[] TRANSFORMS_Y = new TRSRTransformation[] {
+            new TRSRTransformation(ModelRotation.X0_Y0), new TRSRTransformation(ModelRotation.X0_Y180),
+            new TRSRTransformation(ModelRotation.X0_Y270), new TRSRTransformation(ModelRotation.X0_Y90) };
+
+    // From NORTH to DOWN, UP, NORTH, SOUTH
+    private static final TRSRTransformation[] TRANSFORMS_X = new TRSRTransformation[] {
+            new TRSRTransformation(ModelRotation.X90_Y90), new TRSRTransformation(ModelRotation.X270_Y270),
+            new TRSRTransformation(ModelRotation.X90_Y270), new TRSRTransformation(ModelRotation.X270_Y90) };
+
+    // From NORTH to DOWN, UP, WEST, EAST
+    private static final TRSRTransformation[] TRANSFORMS_Z = new TRSRTransformation[] {
+            new TRSRTransformation(ModelRotation.X90_Y180), new TRSRTransformation(ModelRotation.X270_Y0),
+            new TRSRTransformation(ModelRotation.X270_Y180), new TRSRTransformation(ModelRotation.X90_Y0) };
+
+    private static final Map<IBlockState, ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>>> QUAD_CACHE = new HashMap<>();
+    private static final Map <IBlockState, ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>>> QUAD_CACHE_ITEMS = new HashMap<>();
     private final IModel baseModel;
     private final IModel sideModelBase;
     private final IModel sideModelInput;
     private final IModel sideModelOutput;
     private final IModel[] cornerModels;
+    private final IModelState modelState;
     private final VertexFormat format;
     private final IBakedModel bakedBaseModel;
     private final Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter;
     private final TextureAtlasSprite particle;
+    private final BlockPipe.PipeType type;
 
-    private ModelPipeBaked(IModel baseModel, IModel sideModelBase, IModel sideModelInput, IModel sideModelOutput, IModel[] cornerModels,
-            ImmutableMap<String, String> textures, IModelState modelState, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter)
+    private ModelPipeBaked(
+            BlockPipe.PipeType type,
+            IModel baseModel,
+            IModel sideModelBase,
+            IModel sideModelInput,
+            IModel sideModelOutput,
+            IModel[] cornerModels,
+            ImmutableMap<String, String> textures,
+            IModelState modelState,
+            VertexFormat format,
+            Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter)
     {
+        this.type = type;
         this.baseModel       = baseModel      .retexture(ImmutableMap.of("texture", textures.get("pipe_base")));
         this.sideModelBase   = sideModelBase  .retexture(ImmutableMap.of("texture", textures.get("pipe_base")));
         this.sideModelInput  = sideModelInput .retexture(ImmutableMap.of("texture", textures.get("side_input")));
         this.sideModelOutput = sideModelOutput.retexture(ImmutableMap.of("texture", textures.get("side_output")));
         this.cornerModels = new IModel[cornerModels.length];
+        this.modelState = modelState;
 
         for (int i = 0; i < this.cornerModels.length; i++)
         {
@@ -122,34 +149,48 @@ public class ModelPipeBaked implements IBakedModel
     @Override
     synchronized public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
     {
+        ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>> quads;
+
         // Item model
         if (state == null)
         {
-            state = AutoverseBlocks.PIPE.getDefaultState();
-            //return this.bakedBaseModel.getQuads(state, side, rand);
-        }
+            state = AutoverseBlocks.PIPE.getDefaultState().withProperty(BlockPipe.TYPE, this.type);
 
-        ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>> quads = QUAD_CACHE.get(state);
+            quads = QUAD_CACHE_ITEMS.get(state);
 
-        if (quads == null)
-        {
-            IModelState modelState = new TRSRTransformation(ModelRotation.X0_Y0);
-            IBakedModel bakedBaseModel = this.baseModel.bake(modelState, this.format, this.bakedTextureGetter);
+            if (quads == null)
+            {
+                TRSRTransformation trsca = new TRSRTransformation(null, null, new javax.vecmath.Vector3f(1.5f, 1.5f, 1.5f), null);
+                IModelState modelState = new ModelStateComposition(this.modelState, TRSRTransformation.blockCenterToCorner(trsca));
 
-            quads = this.bakeFullModel(bakedBaseModel, state, side);
-            QUAD_CACHE.put(state, quads);
+                quads = this.bakeFullModel(state, side, modelState);
+                QUAD_CACHE_ITEMS.put(state, quads);
+            }
 
             return quads.get(Optional.ofNullable(side));
         }
+        else
+        {
+            quads = QUAD_CACHE.get(state);
 
-        return QUAD_CACHE.get(state).get(Optional.ofNullable(side));
+            if (quads == null)
+            {
+                TRSRTransformation modelState = new TRSRTransformation(ModelRotation.X0_Y0);
+
+                quads = this.bakeFullModel(state, side, modelState);
+                QUAD_CACHE.put(state, quads);
+            }
+
+            return quads.get(Optional.ofNullable(side));
+        }
     }
 
-    private ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>> bakeFullModel(IBakedModel baseModel, IBlockState state, @Nullable EnumFacing side)
+    private ImmutableMap<Optional<EnumFacing>, ImmutableList<BakedQuad>> bakeFullModel(
+            IBlockState state, @Nullable EnumFacing side, @Nullable IModelState modelState)
     {
         ImmutableMap.Builder<Optional<EnumFacing>, ImmutableList<BakedQuad>> builder = ImmutableMap.builder();
-        List<IBakedModel> sideModels = this.getSideModels(state);
-        List<IBakedModel> middleModels = this.getMiddleModelPieces(state);
+        List<IBakedModel> sideModels = this.getSideModels(state, modelState);
+        List<IBakedModel> middleModels = this.getMiddleModelPieces(state, modelState);
 
         for (EnumFacing face : MODEL_FACES)
         {
@@ -178,45 +219,34 @@ public class ModelPipeBaked implements IBakedModel
         return builder.build();
     }
 
-    private List<IBakedModel> getMiddleModelPieces(IBlockState state)
+    private List<IBakedModel> getMiddleModelPieces(IBlockState state, @Nullable IModelState modelStateIn)
     {
         List<IBakedModel> models = new ArrayList<IBakedModel>();
 
-        this.addMiddleModelPieces(state, PositionUtils.SIDES_Y, EnumFacing.UP, models);
-        this.addMiddleModelPieces(state, PositionUtils.SIDES_X, EnumFacing.EAST, models);
-        this.addMiddleModelPieces(state, PositionUtils.SIDES_Z, EnumFacing.SOUTH, models);
+        this.addMiddleModelPieces(state, PositionUtils.SIDES_Y, EnumFacing.UP, models, modelStateIn);
+        this.addMiddleModelPieces(state, PositionUtils.SIDES_X, EnumFacing.EAST, models, modelStateIn);
+        this.addMiddleModelPieces(state, PositionUtils.SIDES_Z, EnumFacing.SOUTH, models, modelStateIn);
 
-        this.addCornerPieces(state, models, EnumFacing.UP, null);
-        this.addCornerPieces(state, models, EnumFacing.DOWN, new TRSRTransformation(ModelRotation.X180_Y180));
+        this.addCornerPieces(state, models, EnumFacing.UP, modelStateIn);
+
+        TRSRTransformation tr = new TRSRTransformation(ModelRotation.X180_Y180);
+        IModelState modelState = modelStateIn != null ? new ModelStateComposition(tr, modelStateIn) : tr;
+        this.addCornerPieces(state, models, EnumFacing.DOWN, modelState);
 
         return models;
     }
 
-    private void addMiddleModelPieces(IBlockState state, EnumFacing[] sides, EnumFacing rotationAxis, List<IBakedModel> models)
+    /**
+     * Transforms a single vertical pipe model edge (strip) to all four sides around the given <b>rotationAxis</b>.
+     * This method is then called for each of the three axes separately.
+     * @param state
+     * @param sides
+     * @param rotationAxis
+     * @param models
+     */
+    private void addMiddleModelPieces(IBlockState state, EnumFacing[] sides, EnumFacing rotationAxis,
+            List<IBakedModel> models, @Nullable IModelState modelStateIn)
     {
-        // NORTH, SOUTH, WEST, EAST
-        TRSRTransformation[] trY = new TRSRTransformation[] {
-                new TRSRTransformation(ModelRotation.X0_Y0),
-                new TRSRTransformation(ModelRotation.X0_Y180),
-                new TRSRTransformation(ModelRotation.X0_Y270),
-                new TRSRTransformation(ModelRotation.X0_Y90)
-        };
-
-        // DOWN, UP, NORTH, SOUTH
-        TRSRTransformation[] trX = new TRSRTransformation[] {
-                new TRSRTransformation(ModelRotation.X90_Y90),
-                new TRSRTransformation(ModelRotation.X270_Y270),
-                new TRSRTransformation(ModelRotation.X90_Y270),
-                new TRSRTransformation(ModelRotation.X270_Y90)
-        };
-
-        // DOWN, UP, WEST, EAST
-        TRSRTransformation[] trZ = new TRSRTransformation[] {
-                new TRSRTransformation(ModelRotation.X90_Y180),
-                new TRSRTransformation(ModelRotation.X270_Y0),
-                new TRSRTransformation(ModelRotation.X270_Y180),
-                new TRSRTransformation(ModelRotation.X90_Y0)
-        };
         int i = 0;
 
         for (EnumFacing side : sides)
@@ -224,35 +254,49 @@ public class ModelPipeBaked implements IBakedModel
             EnumFacing other = PositionUtils.rotateAround(side, rotationAxis);
             boolean conn1 = state.getValue(BlockPipe.CONNECTIONS.get(side.getIndex())) != Connection.NONE;
             boolean conn2 = state.getValue(BlockPipe.CONNECTIONS.get(other.getIndex())) != Connection.NONE;
+
             TRSRTransformation tr;
-            if (sides == PositionUtils.SIDES_Y)         { tr = trY[i]; }
-            else if (sides == PositionUtils.SIDES_X)    { tr = trX[i]; }
-            else                                        { tr = trZ[i]; }
+            if (sides == PositionUtils.SIDES_Y)         { tr = TRANSFORMS_Y[i]; }
+            else if (sides == PositionUtils.SIDES_X)    { tr = TRANSFORMS_X[i]; }
+            else                                        { tr = TRANSFORMS_Z[i]; }
+
+            IModelState modelState = modelStateIn != null ? new ModelStateComposition(tr, modelStateIn) : tr;
+
+            /*
+            boolean conn3 = state.getValue(BlockPipe.CONNECTIONS.get(rotationAxis.getIndex())) != Connection.NONE;
+            boolean conn4 = state.getValue(BlockPipe.CONNECTIONS.get(rotationAxis.getOpposite().getIndex())) != Connection.NONE;
+            // When there is no connection on either of the sides around an edge,
+            // or when there is a connection on both sides,
+            // or in a three-way intersection along the rotation axis,
+            // then a connecting model strip is added to that edge.
+            if ((conn1 == conn2) || (conn1 && conn3 && conn4))
+            */
 
             // When there is no connection on either of the sides around an edge,
-            // or when there is a connection on both sides, then a vertical strip is added to that edge.
-            //if (conn1 == conn2)
+            // or when there is a connection on both sides,
+            // then a connecting model strip is added to that edge.
+            if (conn1 == conn2)
 
             // No connection on either side
-            if (conn1 == false && conn2 == false)
+            //if (conn1 == false && conn2 == false)
             {
-                models.add(this.baseModel.bake(tr, this.format, this.bakedTextureGetter));
+                models.add(this.baseModel.bake(modelState, this.format, this.bakedTextureGetter));
             }
 
             i++;
         }
     }
 
-    private void addCornerPieces(IBlockState state, List<IBakedModel> models, EnumFacing upAxis, @Nullable TRSRTransformation initialRot)
+    /**
+     * Select the correct pipe corner model based on which of the three adjacent sides have pipe connections on them.
+     * The selected model is then transformed for all four corners on that "plane" (either the top of the pipe, or the bottom).
+     * @param state
+     * @param models
+     * @param upAxis
+     * @param modelState
+     */
+    private void addCornerPieces(IBlockState state, List<IBakedModel> models, EnumFacing upAxis, @Nullable IModelState modelStateIn)
     {
-        // NORTH, SOUTH, WEST, EAST
-        TRSRTransformation[] trY = new TRSRTransformation[] {
-                new TRSRTransformation(ModelRotation.X0_Y0),
-                new TRSRTransformation(ModelRotation.X0_Y180),
-                new TRSRTransformation(ModelRotation.X0_Y270),
-                new TRSRTransformation(ModelRotation.X0_Y90)
-        };
-
         int i = 0;
 
         for (EnumFacing side : PositionUtils.SIDES_Y)
@@ -275,33 +319,36 @@ public class ModelPipeBaked implements IBakedModel
                 index |= 0x4;
             }
 
-            TRSRTransformation tr = initialRot != null ? trY[i].compose(initialRot) : trY[i];
-            models.add(this.cornerModels[index].bake(tr, this.format, this.bakedTextureGetter));
+            IModelState modelState = modelStateIn != null ? new ModelStateComposition(TRANSFORMS_Y[i], modelStateIn) : TRANSFORMS_Y[i];
+
+            models.add(this.cornerModels[index].bake(modelState, this.format, this.bakedTextureGetter));
 
             i++;
         }
     }
 
-    private List<IBakedModel> getSideModels(IBlockState state)
+    private List<IBakedModel> getSideModels(IBlockState state, @Nullable IModelState modelStateIn)
     {
         List<IBakedModel> models = new ArrayList<IBakedModel>();
 
         for (EnumFacing side : EnumFacing.values())
         {
             BlockPipe.Connection conn = state.getValue(BlockPipe.CONNECTIONS.get(side.getIndex()));
+            TRSRTransformation tr = new TRSRTransformation(side);
+            IModelState modelState = modelStateIn != null ? new ModelStateComposition(tr, modelStateIn) : tr;
 
             switch (conn)
             {
                 case BASIC:
-                    models.add(this.sideModelBase.bake(new TRSRTransformation(side), this.format, this.bakedTextureGetter));
+                    models.add(this.sideModelBase.bake(modelState, this.format, this.bakedTextureGetter));
                     break;
 
                 case EXTRACT:
-                    models.add(this.sideModelInput.bake(new TRSRTransformation(side), this.format, this.bakedTextureGetter));
+                    models.add(this.sideModelInput.bake(modelState, this.format, this.bakedTextureGetter));
                     break;
 
                 case OUTPUT:
-                    models.add(this.sideModelOutput.bake(new TRSRTransformation(side), this.format, this.bakedTextureGetter));
+                    models.add(this.sideModelOutput.bake(modelState, this.format, this.bakedTextureGetter));
                     break;
 
                 default:
@@ -318,7 +365,13 @@ public class ModelPipeBaked implements IBakedModel
         private static final ResourceLocation SIDE_MODEL_INPUT  = new ResourceLocation(Reference.MOD_ID, "block/pipe_side_input");
         private static final ResourceLocation SIDE_MODEL_OUTPUT = new ResourceLocation(Reference.MOD_ID, "block/pipe_side_output");
 
+        protected final BlockPipe.PipeType type;
         protected final Map<String, String> textures = new HashMap<String, String>();
+
+        protected ModelPipe(BlockPipe.PipeType type)
+        {
+            this.type = type;
+        }
 
         @Override
         public IModelState getDefaultState()
@@ -379,7 +432,7 @@ public class ModelPipeBaked implements IBakedModel
                 Autoverse.logger.warn("Failed to load a model for the Pipe!", e);
             }
 
-            return new ModelPipeBaked(baseModel, sideModelBase, sideModelInput, sideModelOutput, cornerModels,
+            return new ModelPipeBaked(this.type, baseModel, sideModelBase, sideModelInput, sideModelOutput, cornerModels,
                     this.getTextureMapping(), state, format, bakedTextureGetter);
         }
 
@@ -393,6 +446,7 @@ public class ModelPipeBaked implements IBakedModel
     {
         private ModelPipeBasic()
         {
+            super(BlockPipe.PipeType.BASIC);
             this.textures.put("pipe_base",   TEX_BASIC);
             this.textures.put("side_input",  TEX_SIDE_INPUT);
             this.textures.put("side_output", TEX_SIDE_OUTPUT);
@@ -403,6 +457,7 @@ public class ModelPipeBaked implements IBakedModel
     {
         private ModelPipeExtraction()
         {
+            super(BlockPipe.PipeType.EXTRACTION);
             this.textures.put("pipe_base",   TEX_EXTRACTION);
             this.textures.put("side_input",  TEX_SIDE_INPUT);
             this.textures.put("side_output", TEX_SIDE_OUTPUT);
@@ -413,6 +468,7 @@ public class ModelPipeBaked implements IBakedModel
     {
         private ModelPipeDirectional()
         {
+            super(BlockPipe.PipeType.DIRECTIONAL);
             this.textures.put("pipe_base",   TEX_DIRECTIONAL);
             this.textures.put("side_input",  TEX_SIDE_INPUT);
             this.textures.put("side_output", TEX_SIDE_OUTPUT);
@@ -454,6 +510,7 @@ public class ModelPipeBaked implements IBakedModel
         public void onResourceManagerReload(IResourceManager resourceManager)
         {
             QUAD_CACHE.clear();
+            QUAD_CACHE_ITEMS.clear();
         }
     }
 
