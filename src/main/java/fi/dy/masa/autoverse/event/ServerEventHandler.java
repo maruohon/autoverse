5e4eda0be02bdda7b58292bff54c356dc48d0948
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-07-25 02:39:42 +0300

    Pipes: Add an event handler to allow sneak + left clicking in Creative mode

--- src/main/java/fi/dy/masa/autoverse/event/ServerEventHandler.java
+++ src/main/java/fi/dy/masa/autoverse/event/ServerEventHandler.java
@@ -1,9 +1,12 @@
 package fi.dy.masa.autoverse.event;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import fi.dy.masa.autoverse.block.base.AutoverseBlocks;
 import fi.dy.masa.autoverse.util.PlacementProperties;
 
 public class ServerEventHandler
@@ -20,6 +23,21 @@ public class ServerEventHandler
         if (event.player instanceof EntityPlayerMP)
         {
             PlacementProperties.getInstance().syncAllDataForPlayer((EntityPlayerMP) event.player);
+        }
+    }
+
+    @SubscribeEvent
+    public void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event)
+    {
+        if (event.getEntityPlayer().capabilities.isCreativeMode && event.getEntityPlayer().isSneaking())
+        {
+            Block block = event.getWorld().getBlockState(event.getPos()).getBlock();
+
+            if (block == AutoverseBlocks.PIPE)
+            {
+                block.onBlockClicked(event.getWorld(), event.getPos(), event.getEntityPlayer());
+                event.setCanceled(true);
+            }
         }
     }
 }
