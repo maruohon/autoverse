f547fcf49fb6304e72a66c77807c25f48c254315
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-08-01 23:21:25 +0300

    Add JAR signing

--- src/main/java/fi/dy/masa/autoverse/Autoverse.java
+++ src/main/java/fi/dy/masa/autoverse/Autoverse.java
@@ -1,9 +1,11 @@
 package fi.dy.masa.autoverse;
 
 import java.util.Random;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
@@ -15,7 +17,7 @@ import fi.dy.masa.autoverse.proxy.CommonProxy;
 import fi.dy.masa.autoverse.reference.Reference;
 import fi.dy.masa.autoverse.util.PlacementProperties;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.autoverse.config.AutoverseGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/autoverse/master/update.json",
     acceptedMinecraftVersions = "[1.12]")
@@ -27,14 +29,12 @@ public class Autoverse
     @SidedProxy(clientSide = "fi.dy.masa.autoverse.proxy.ClientProxy", serverSide = "fi.dy.masa.autoverse.proxy.CommonProxy")
     public static CommonProxy proxy;
 
-    public static Logger logger;
-
+    public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
     public static final Random RAND = new Random(System.currentTimeMillis());
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
         Configs.loadConfigsFromFile(event.getSuggestedConfigurationFile());
         PacketHandler.init();
 
@@ -51,5 +51,24 @@ public class Autoverse
         PlacementProperties.getInstance().readFromDisk();
 
         event.registerServerCommand(new CommandLoadConfigs());
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
--- src/main/java/fi/dy/masa/autoverse/reference/Reference.java
+++ src/main/java/fi/dy/masa/autoverse/reference/Reference.java
@@ -5,4 +5,5 @@ public class Reference
     public static final String MOD_ID = "autoverse";
     public static final String MOD_NAME = "Autoverse";
     public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String FINGERPRINT = "2b03e1423915a189b8094816baa18f239d576dff";
 }
